a976907c39a699850a88684b7f10bf59f4869ab1
Niketan <niketanb1234@gmail.com>
2023-10-04 12:12:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "jspdf-autotable";
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import "react-data-table-component-extensions/dist/index.css";
+import html2canvas from "html2canvas";
 // import DataTable from 'react-data-table-component';
 // import "bootstrap/dist/css/bootstrap.css";
 
@@ -15,7 +16,7 @@ import { Modal, Button } from "react-bootstrap";
 import dist from "react-data-table-component-extensions";
 function App() {
   const [Employes, setEmployes] = useState([]);
-  const [TpData, settpData] = useState([]);
+  const [TpData, setTpData] = useState([]);
   const [checkedEmployes, setcheckedEmployes] = useState([]);
   const [deleteOnCheck, setdelteonCheck] = useState([]);
   const [flag, setflag] = useState(0);
@@ -31,140 +32,254 @@ function App() {
       },
     });
     const json = await response.json();
-    
     setEmployes(json);
-    settpData(json);
-  };
-  const onChange = async () => {
+    setTpData(json);
     
-    let division=document.getElementById("division")
-    let district=document.getElementById("district")
-    let post=document.getElementById("post")
-    division=division.value;
-    district=district.value;
-    post=post.value;
-    var searchData=[];
-   
-    if(division!="All")
+  };
+  const rada=(division)=>{
+    if(division=="All")
     {
-      
-        if(district!="All")
-        {
-          
-          if(post!="All")
-          {
-            for(let i=0;i<TpData.length;i++)
-            {
-              if(TpData[i].Division==division && TpData[i].District==district && TpData[i].Post==post)
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-          }
-          else
-          {
-            for(let i=0;i<TpData.length;i++)
-            {
-              if(TpData[i].Division==division && TpData[i].District==district )
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-          }
-        }
-        else
-        {
-          if(post!="All")
-          {
-            
-            for(let i=0;i<TpData.length;i++)
-            {
-              if(TpData[i].Division==division && post==TpData[i].Post )
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-          }
-          else
-          {
-            
-            for(let i=0;i<TpData.length;i++)
-            {
-      
-              if(TpData[i].Division==division)
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-          }
-        }
+      setdistrict(All);
     }
-    else
+    if(division=="Pune")
     {
-      if(district!="All")
-      {
-        
-        if(post!="All")
-        {
-          
-          for(let i=0;i<TpData.length;i++)
-            {
-              if(TpData[i].District==district && post==TpData[i].Post )
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-        }
-        else
-        {
-          
-          for(let i=0;i<TpData.length;i++)
-            {
-              if(TpData[i].District==district)
-              {
-                searchData.push(TpData[i]);
-              }
-            }
-        }
-      }
-      else
-      {
-        
-        if(post!="All")
-        {
-          for(let i=0;i<TpData.length;i++)
-            {
-              // console.log(TpData[i].Post)
-              if( post==TpData[i].Post )
-              {
+      setdistrict(Pune);
+    }
+    else if(division=="Nashik")
+    {
+      setdistrict(Nashik);
+    }
+    else if(division=="Nagpur")
+    {
+      setdistrict(Nagpur)
+    }
+    else if(division=="Konkan-2")
+    {
 
-                searchData.push(TpData[i]);
-              }
+    }
+    else if(division=="Konkan-1")
+    {
+      
+    }
+    else if(division=="Chhatrapati Sambhajinagar")
+    {
+      setdistrict(Aurangabad);
+    }
+    else if(division=="Amravati")
+    {
+      setdistrict(Amravati);
+    }
+  }
+  const onChange = async () => {
+    console.log(TpData)
+    let division = document.getElementById("division");
+    let district = document.getElementById("district");
+   
+    let post = document.getElementById("post");
+    division = division.value;
+    district = district.value;
+    
+    post = post.value;
+    var searchData = [];
+    rada(division);
+    if (division != "All") {
+      
+      if (district != "All") {
+       
+        if (post != "All") {
+          for (let i = 0; i < TpData.length; i++) {
+            if (
+              TpData[i].Division == division &&
+              TpData[i].District == district &&
+              TpData[i].Post == post
+            ) {
+              searchData.push(TpData[i]);
             }
+          }
+        } else {
+          
+          for (let i = 0; i < TpData.length; i++) {
+            if (
+              TpData[i].Division == division &&
+              TpData[i].District == district
+            ) {
+              
+              searchData.push(TpData[i]);
+            }
+          }
         }
-        else
-        {
-          for(let i=0;i<TpData.length;i++)
-            {
-                searchData.push(TpData[i]);
+      } else {
+        if (post != "All") {
+          for (let i = 0; i < TpData.length; i++) {
+            if (TpData[i].Division == division && post == TpData[i].Post) {
+              searchData.push(TpData[i]);
             }
+          }
+        } else {
+          for (let i = 0; i < TpData.length; i++) {
+            if (TpData[i].Division == division) {
+              searchData.push(TpData[i]);
+              
+            }
+          }
+        }
+      }
+    } else {
+      if (district != "All") {
+        
+        if (post != "All") {
+          for (let i = 0; i < TpData.length; i++) {
+            if (TpData[i].District == district && post == TpData[i].Post) {
+              searchData.push(TpData[i]);
+            }
+          }
+        } else {
+          
+          for (let i = 0; i < TpData.length; i++) {
+            console.log(district)
+            if (TpData[i].District == district) {
+              searchData.push(TpData[i]);
+            }
+          }
+        }
+      } else {
+        if (post != "All") {
+          for (let i = 0; i < TpData.length; i++) {
+           
+            if (post == TpData[i].Post) {
+              searchData.push(TpData[i]);
+            }
+          }
+        } else {
+          
+          for (let i = 0; i < TpData.length; i++) {
+            searchData.push(TpData[i]);
+            
+          }
         }
       }
     }
-  
+
     setEmployes(searchData);
   };
-  
+  const Konkan1=(
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Palghar">Palghar </option>
+      <option value="Raigad">Raigad </option>
+      <option value="Ratnagiri">Ratnagiri </option>
+      <option value="Sindhudurg">Sindhudurg </option>
+    </select>
+  );
+  const Konkan2=(
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Mumbai">Mumbai </option>
+      <option value="Thane">Thane </option>
+    </select>
+  );
+  const Pune = (
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="satara">Satara </option>
+      <option value="Sangli">Sangli </option>
+      <option value="Solapur">Solapur </option>
+      <option value="Pune">Pune </option>
+      <option value="Kolhapur">Kolhapur </option>
+    </select>
+  );
+  const Amravati = (
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Akola">Akola </option>
+      <option value="Amravati">Amravati </option>
+      <option value="Buldhana">Buldhana </option>
+      <option value="Yavatmal">Yavatmal </option>
+      <option value="Washim">Washim </option>
+    </select>
+  );
+  const Aurangabad = (
+    
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Chhatrapati Sambhajinagar">Chatrapati SambhajiNagar </option>
+      <option value="Beed">Beed </option>
+      <option value="Jalna">Jalna </option>
+      <option value="Dharashiva">Dharashiva </option>
+      <option value="Nanded">Nanded </option>
+      <option value="Latur">Latur </option>
+      <option value="Parbhani">Parbhani </option>
+      <option value="Hingoli">Hingoli </option>
+    </select>
+  );
+  const Nagpur= (
 
-  useEffect(() => {
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Bhandara">Bhandara </option>
+      <option value="Chandrapur">Chandrapur </option>
+      <option value="Gadchiroli">Gadchiroli </option>
+      <option value="Gondia">Gondia </option>
+      <option value="Nagpur">Nagpur </option>
+      <option value="Wardha">Wardha </option>
+    </select>
+  );
+  const Nashik= (
+ 
+    <select id="district" onChange={onChange}>
+      <option value="All">All </option>
+      <option value="Ahmednagar">Ahmednagar </option>
+      <option value="Dhule">Dhule </option>
+      <option value="Jalgaon">Jalgaon </option>
+      <option value="Nandurbar">Nandurbar </option>
+      <option value="Nashik">Nashik </option>
+    </select>
+  );
+  const All=(<select name="cars" id="district" onChange={onChange}>
+    
+  <option value="All">All</option>
+  <option value="Ahmednagar">Ahmednagar</option>
+  <option value="Akola">Akola</option>
+  <option value="Chhatrapati Sambhajinagar">
+    Chhatrapati Sambhajinagar
+  </option>
+  <option value="Beed">Beed</option>
+  <option value="Bhandara">Bhandara</option>
+  <option value="Buldhana">Buldhana </option>
+  <option value="Chandrapur">Chandrapur </option>
+  <option value="Dhule">Dhule </option>
+  <option value="Dharashiva">Dharashiva </option>
+  <option value="Gadchiroli">Gadchiroli </option>
+  <option value="Gondia">Gondia </option>
+  <option value="Hingoli">Hingoli </option>
+  <option value="Jalgaon">Jalgaon </option>
+  <option value="Jalna">Jalna </option>
+  <option value="Kolhapur">Kolhapur </option>
+  <option value="Latur">Latur </option>
+  <option value="Mumbai">Mumbai </option>
+  <option value="Palghar">Palghar </option>
+  <option value="parbhani">parbhani </option>
+  <option value="Pune">Pune </option>
+  <option value="Raigad">Raigad </option>
+  <option value="Ratnagiri">Ratnagiri </option>
+  <option value="Satara">Satara </option>
+  <option value="Sangli">Sangli </option>
+  <option value="Solapur">Solapur </option>
+  <option value="Sindhudurg">Sindhudurg </option>
+  <option value="Thane">Thane </option>
+  <option value="Washim">Washim </option>
+  <option value="Wardha">Wardha </option>
+  <option value="Yavatmal">Yavatmal </option>
+</select>)
+const [district,setdistrict]=useState();
+  useEffect( () => {
     onClickSubmit();
     setflag(0);
+    
   }, []);
 
   const hadnleRowChange = (event) => {
     setcheckedEmployes(event.selectedRows);
-
-  
   };
   const columns = [
     {
@@ -201,7 +316,7 @@ function App() {
       name: (
         <div>
           Post
-          <select  onChange={onChange} id="post">
+          <select onChange={onChange} id="post">
             <option value="All">All</option>
             <option value="Director">Director</option>
             <option value="JDTP">JDTP</option>
@@ -253,15 +368,14 @@ function App() {
       name: (
         <div>
           Division
-          {/* <input type="text" onChange={onChange} style={{ width: "80%" }} />
-           */}
           <select name="cars" id="division" onChange={onChange}>
             <option value="All">All</option>
             <option value="Nagpur">Nagpur</option>
             <option value="Amravati">Amravati</option>
             <option value="Pune">Pune</option>
+            <option value="Konkan-1">Konkan-1</option>
             <option value="Konkan-2">Konkan-2</option>
-            <option value="Aurangabad">Aurangabad</option>
+            <option value="Chhatrapati Sambhajinagar">Chatrapati SambhajiNagar</option>
             <option value="Nashik">Nashik</option>
           </select>
         </div>
@@ -276,52 +390,16 @@ function App() {
       name: (
         <div>
           District
-          {/* <input type="text" onChange={onChange} style={{ width: "80%" }} />
-           */}
-          <select name="cars" id="district" onChange={onChange}>
-            <option value="All">All</option>
-            <option value="Ahmednagar">Ahmednagar</option>
-            <option value="Acola">Acola</option>
-            <option value="Chhatrapati Sambhajinagar">Chhatrapati Sambhajinagar</option>
-            <option value="Beed">Beed</option>
-            <option value="Bhandara">Bhandara</option>
-            <option value="Buldhana">Buldhana	</option>
-            <option value="Chandrapur">Chandrapur	</option>
-            <option value="Dhule">Dhule	</option>
-            <option value="Dharashiva">Dharashiva	</option>
-            <option value="Gadchiroli">Gadchiroli	</option>
-            <option value="Gondia">Gondia	</option>
-            <option value="Hingoli">Hingoli	</option>
-            <option value="Jalgaon">Jalgaon	</option>
-            <option value="Jalna">Jalna	</option>
-            <option value="Kolhapur">Kolhapur	</option>
-            <option value="Latur">Latur	</option>
-            <option value="Mumbai">Mumbai	</option>
-            <option value="Osmanabad">Osmanabad	</option>
-            <option value="Palghar">Palghar	</option>
-            <option value="parbhani">parbhani	</option>
-            <option value="Pune">Pune	</option>
-            <option value="Raigad">Raigad	</option>
-            <option value="Ratnagiri">Ratnagiri		</option>
-            <option value="Satara">Satara		</option>
-            <option value="sangli">Sangli		</option>
-            <option value="Solapur">Solapur		</option>
-            <option value="Sindhudurg">Sindhudurg		</option>
-            <option value="Thane">Thane		</option>
-            <option value="Washim">Washim		</option>
-            <option value="Wardha">Wardha		</option>
-            <option value="Yavatmal">Yavatmal		</option>
-          </select>
+          {district?district:All}
         </div>
       ),
       selector: "District",
-   
+
       style: {
         background: "#cbe8ff",
         border: "0.1px solid black",
       },
     },
-
   ];
 
   const data = Employes;
@@ -332,7 +410,6 @@ function App() {
     data,
   };
   const addNames = () => {
-    
     setEmployes(checkedEmployes);
     setdelteonCheck(checkedEmployes);
     setflag(1);
@@ -350,7 +427,38 @@ function App() {
     orientation: "Landscape",
     documentTitle: "Userdata",
   });
+
+
+  const exportPDF = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "landscape"; // portrait or landscape
+    
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(10);
+
+    const title = "My Awesome Report";
+    const headers = [["Employee Name", "Post","Office Name","ContactNo","MailID","OfficeContactNo","OfficeEMailID"]];
+
+    const data = Employes.map(elt=> [elt.शासकीयकर्मचाऱ्याचेनाव, elt.Post,elt.कार्यालयाचेनाव,elt.संपर्कक्र,elt.ईमेल,elt.कार्यालयाचासंपर्कक्र,elt.कार्यालयाचाईमेल]);
+
+    let content = {
+      // startY: 25,
+      head: headers,
+      body: data,
+      theme: 'grid',
+     
+    };
+
+    // doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("report.pdf")
+  }
+
   const deleteNames = () => {
+    console.log(Employes)
     // Step 1: Identify rows to be deleted
     let updatedData = [];
     for (let i = 0; i < deleteOnCheck.length; i++) {
@@ -381,7 +489,7 @@ function App() {
       </button>
     );
     printList = (
-      <Button variant="success" className="btn" onClick={generatePDF}>
+      <Button variant="success" className="btn" onClick={exportPDF}>
         PrintList
       </Button>
     );
@@ -411,7 +519,7 @@ function App() {
         {printList}
       </div>
       <div ref={conponentPDF}>
-        <DataTableExtensions {...tableData} print={false} export={false} >
+        <DataTableExtensions {...tableData} print={false} export={false}>
           <DataTable
             actions={[
               {
